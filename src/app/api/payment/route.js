231bf5ff4d85638a5d2eedf0a3efd31ed313c788
Nico <franckennicolas@gmail.com>
2023-08-24 22:13:51 -0300

    first commit

---
+++ src/app/api/payment/route.js
@@ -0,0 +1,21 @@
+import mercadopago from "mercadopago";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  mercadopago.configure({
+    access_token:
+      "TEST-8758694386882879-082419-5831537b46055c9568c9865013ec300c-1459700923",
+  });
+
+  const result = await mercadopago.preferences.create({
+    items: [{ title: "PC", unit_price: 500, currency_id: "ARS", quantity: 1 }],
+    back_urls: {
+      success: "http://localhost:3000/success",
+      failure: "http://localhost:3000/api/failure",
+      pending: "http://localhost:3000/api/pending",
+    },
+    notification_url: "https://3c53-45-176-89-34.ngrok.io/api/webhook",
+  });
+
+  return NextResponse.json(result.body);
+}
---
+++ src/app/api/webhook/route.js
@@ -0,0 +1,20 @@
+import mercadopago from "mercadopago";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  const payment = await req.json();
+
+  console.log(payment);
+
+  try {
+    if (payment.type === "payment") {
+      const data = await mercadopago.payment.findById(payment.data.id);
+      console.log(data);
+    }
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json({ message: "ERROR" });
+  }
+
+  return NextResponse.json({ message: "ok" });
+}
